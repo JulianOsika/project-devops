4927a75c63d804a224afb5335a9e6a266cc2bd21
Julian Osika <ubuntu@ip-172-31-39-140.eu-north-1.compute.internal>
2026-01-03 14:03:01 +0000

    Implementacja Docker Compose z podzialem na serwisy

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from redis import Redis
+import os
 
 app = Flask(__name__)
-# Timeout 1 sekunda - klucz do sukcesu
-redis = Redis(host='127.0.0.1', port=6379, socket_timeout=1, socket_connect_timeout=1)
+
+# W Dockerze nazwa hosta to nazwa serwisu z docker-compose, czyli 'redis'
+redis = Redis(host='redis', port=6379, socket_timeout=1, socket_connect_timeout=1)
 
 @app.route('/')
 def hello():
     try:
         count = redis.incr('hits')
-    except Exception:
-        count = "brak polaczenia z Redis"
-    return 'Witaj! To jest projekt DevOps. Tę stronę odwiedzono {} razy.\n'.format(count)
+    except Exception as e:
+        count = "brak polaczenia z Redis: {}".format(str(e))
+    return 'Witaj! To jest projekt w Docker Compose. Te strone odwiedzono {} razy.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
